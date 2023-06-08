f5e5c5d0d6c11e133ce8bb9e2b00e1183bf229d5
Elité <111225087+el3i7e@users.noreply.github.com>
2023-06-08 19:16:46 +0200

    Update copier.ps1

--- copier.ps1
+++ copier.ps1
@@ -1,15 +1,12 @@
-$sourceDirectory = "C:\"  # Replace with the root directory you want to search
 $destinationDirectory = "D:\"
-
-# Calculate the date from one month ago
 $startDate = (Get-Date).AddMonths(-1)
 
 # Get all .docx files modified within the last month from any directory
-$files = Get-ChildItem -Path $sourceDirectory -Filter "*.docx" -Recurse |
+$files = Get-ChildItem -Recurse -Filter "*.docx" |
          Where-Object { $_.LastWriteTime -ge $startDate }
 
-# Copy each .docx file to the destination directory
+# Copy all .docx files to the destination directory
 foreach ($file in $files) {
-    $destinationPath = Join-Path -Path $destinationDirectory -ChildPath $file.FullName.Substring($sourceDirectory.Length)
+    $destinationPath = Join-Path -Path $destinationDirectory -ChildPath $file.FullName.Substring(3)
     Copy-Item -Path $file.FullName -Destination $destinationPath
 }
